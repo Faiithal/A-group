16b9480a3d9301868026fa4acfe540c408ac6ce3
faiithal <johndumlao2355@gmail.com>
2024-08-19 09:02:12 +0800

    fixed broken link

--- index.php
+++ index.php
@@ -1,12 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+    $michael_pic = "/Pictures/Mikel.jpg";
+    $joshua_pic = "/Pictures/Joshua.jpg";
+    $hillary_pic = "/Pictures/Hillary.jpg";
+    $zoe_pic = "/Pictures/Zoe.jpg";
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=dfdfdf">
 </head>
+
+<style>
+    .one {
+    background: url("<?= $michael_pic?>");
+    background-size: cover;
+}
+
+.two {
+    background: url("<?= $joshua_pic?>");
+    background-size: cover;
+
+}
+
+.three {
+    background: url("<?= $hillary_pic?>");
+    background-size: cover;
+
+}
+
+.four {
+    background: url("<?= $zoe_pic?>");
+    background-size: cover;
+
+}
+</style>
 
 <body>
     <header>
@@ -21,7 +53,7 @@
             <section>
                 <div class="pfp one"></div>
                 </div>
-                <li><a href="Dumlao/Dumlao.php">Michael Dumlao</a></li>
+                <li><a href="./Dumlao/Dumlao.php">Michael Dumlao</a></li>
             </section>
             <section>
                 <div class="pfp two">
@@ -36,7 +68,7 @@
             <section>
                 <div class="pfp four">
                 </div>
-                <li><a href="Zoe/portfolio.php"></a>Zoe Gabriel</a></li>
+                <li><a href="Zoe/portfolio.php">Zoe Gabriel</a></li>
             </section>
         </ul>
     </main>
