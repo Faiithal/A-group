b9a8a26726b9653c043a1796a5204609a4ef25d4
faiithal <johndumlao2355@gmail.com>
2024-09-01 17:47:42 +0800

    Website Overhaul + added new and former members to the site

--- index.php
+++ index.php
@@ -2,75 +2,108 @@
 <html lang="en">
 
 <?php
-    $michael_pic = "/Pictures/Mikel.jpg";
-    $joshua_pic = "/Pictures/Joshua.jpg";
-    $hillary_pic = "/Pictures/Hillary.jpg";
-    $zoe_pic = "/Pictures/Zoe.jpg";
+$michael_pic = "/Pictures/Mikel.jpg";
+$joshua_pic = "/Pictures/Joshua.jpg";
+$hillary_pic = "/Pictures/Hillary.jpg";
+$zoe_pic = "/Pictures/Zoe.jpg";
 ?>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css?v=dfdfdf">
+    <link rel="stylesheet" href="style.css?v=<?= time(); ?>">
 </head>
 
 <style>
-    .one {
-    background: url("<?= $michael_pic?>");
-    background-size: cover;
-}
+    .kel {
+        background: url("<?= $michael_pic ?>");
+        background-size: cover;
+    }
 
-.two {
-    background: url("<?= $joshua_pic?>");
-    background-size: cover;
+    .shu {
+        background: url("<?= $joshua_pic ?>");
+        background-size: cover;
 
-}
+    }
 
-.three {
-    background: url("<?= $hillary_pic?>");
-    background-size: cover;
+    .hillar {
+        background: url("<?= $hillary_pic ?>");
+        background-size: cover;
 
-}
+    }
 
-.four {
-    background: url("<?= $zoe_pic?>");
-    background-size: cover;
+    .zoe {
+        background: url("<?= $zoe_pic ?>");
+        background-size: cover;
 
-}
+    }
 </style>
 
 <body>
     <header>
-        <h1>Group A</h1>
-        <h2>Meet The Members:</h2>
+
     </header>
     <main>
-        <video id="bg" autoplay loop>
-            <source src="video.mp4">
-        </video>
-        <ul>
-            <section>
-                <div class="pfp one"></div>
-                </div>
-                <li><a href="./Dumlao/Dumlao.php">Michael Dumlao</a></li>
-            </section>
-            <section>
-                <div class="pfp two">
-                </div>
-                <li><a href="./Verzosa/Verzosa.php">Joshua Verzosa</a></li>
-            </section>
-            <section>
-                <div class="pfp three">
-                </div>
-                <li><a href="./Mira/Mira.php">Hillary Mira</a></li>
-            </section>
-            <section>
-                <div class="pfp four">
-                </div>
-                <li><a href="Zoe/portfolio.php">Zoe Gabriel</a></li>
-            </section>
-        </ul>
+        <section class="title">
+            <h1>Group A</h1>
+            <p>Get to know our team behind the website</p>
+        </section>
+        <div class="new">
+
+
+            <h2>Current Team Members</h2>
+            <p class="recent_date">As of 8/19/24</p>
+
+            <ul class="current_members">
+                <section>
+                <a href="./Dumlao/Dumlao.php"><div class="pfp kel"></div></a>
+                    <li>Michael Dumlao</li>
+                </section>
+                <section>
+                <a href="./Verzosa/Verzosa.php"><div class="pfp shu"></div></a>
+                    <li>Joshua Verzosa</li>
+                </section>
+                <section>
+                <a href="./Mira/Mira.php"><div class="pfp"></div></a>
+                    <li>Gabriel Evangelista</li>
+                </section>
+                <section>
+                <a href="Zoe/portfolio.php"><div class="pfp"></div></a>
+                    <li>Chamuel Escobar</li>
+                </section>
+            </ul>
+        </div>
+
+        <div class="old">
+            <h2>Former Team Members</h2>
+            <ul class="former_members">
+                <section>
+                    <a href="./Old Members/Elgave/gamba.html">
+                        <div class="pfp five"></div>
+                    </a>
+                    <li>Elgave Gamba</li>
+                </section>
+                <section>
+                    <a href="./Old Members/Mira/index.php">
+                        <div class="pfp hillar"></div>
+                    </a>
+                    <li>Hillary Mira</li>
+                </section>
+                <section>
+                    <a href="./Old Members/Zoe/index.php">
+                        <div class="pfp zoe"></div>
+                    </a>
+                    <li>Zoe Gabriel</li>
+                </section>
+                <section>
+                    <a href="./Old Members/campo.html">
+                        <div class="pfp eight"></div>
+                    </a>
+                    <li>Shekinah Campo</li>
+                </section>
+            </ul>
+        </div>
     </main>
 </body>
 
